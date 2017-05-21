193a8bc87cb8349337c6ca59f27fba60de179a82
Victor Zhong <victor@victorzhong.com>
2017-05-21 15:13:24 -0700

    add manifest, bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='embeddings',
-    version='0.0.1',
+    version='0.0.2',
 
     description='Pretrained word embeddings in Python.',
     long_description=long_description,
