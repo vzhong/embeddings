3f1d0ac76f9d0430db4ac63dd7fa0a8a0482ef78
duongkstn <nguyenduongyht@gmail.com>
2020-01-31 10:20:31 +0700

    add-sqllite-options

--- test/speed_test2.py
+++ test/speed_test2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 import random
 import time
 from nltk.corpus import brown
@@ -9,8 +11,14 @@ import embeddings as E
 if __name__ == '__main__':
     random.seed(0)
     n_samples = 10000
-    k = E.KazumaCharEmbedding()
+    k1 = E.KazumaCharEmbedding(check_same_thread=True)
+    k2 = E.KazumaCharEmbedding(check_same_thread=False)
+
+    g1 = E.GloveEmbedding('common_crawl_840', d_emb=300, show_progress=True, check_same_thread=True)
+    g2 = E.GloveEmbedding('common_crawl_840', d_emb=300, show_progress=True, check_same_thread=False)
 
     for w in ['canada', 'vancouver', 'toronto']:
-        print(k.emb(w))
+        assert(k1.emb(w) == k2.emb(w))
+        assert(g1.emb(w) == g2.emb(w))
+
         
